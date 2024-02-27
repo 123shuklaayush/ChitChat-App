c732d4d5a5c26b76f5ae026c6160ad80a79a3e5d
Ayush Shukla <123shuklaayush@gmail.com>
2024-02-27 17:05:51 +0530

    updated backend url

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chatapp-aklk.onrender.com";
+export const host = "https://chitchat-app-42t1.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
